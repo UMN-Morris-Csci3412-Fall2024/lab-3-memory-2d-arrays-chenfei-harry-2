8d8eef85c51c6ddc8fdfc17c10ca61be6b610a18
Harry <lv000013@morris.umn.edu>
2024-10-10 19:31:46 -0500

    Complete merge sort
    Co-authored-by: Chenfei Peng <peng0368@morris.umn.edu>

--- mergesort/mergesort.c
+++ mergesort/mergesort.c
@@ -1,12 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "mergesort.h"
 
+void merge(int left[], int right[], int values[], int left_size, int right_size) {
+    int i = 0, j = 0, k = 0;
+
+    while (i < left_size && j < right_size) {
+        if (left[i] <= right[j]) {
+            values[k++] = left[i++];
+        } else {
+            values[k++] = right[j++];
+        }
+    }
+
+    while (i < left_size) {
+        values[k++] = left[i++];
+    }
+
+    while (j < right_size) {
+        values[k++] = right[j++];
+    }
+}
+
 void mergesort(int size, int values[]) {
-  // This obviously doesn't actually do any *sorting*, so there's
-  // certainly work still to be done.
-  //
-  // Remember that a key goal here is to learn to use
-  // `malloc/calloc` and `free`, so make sure you explicitly
-  // allocate any new arrays that you need, even if you
-  // might not strictly need to.
-  return;
+    if (size <= 1) {
+        return;
+    }
+
+    int mid = size / 2;
+
+    int* left = (int*) calloc(mid, sizeof(int));
+    int* right = (int*) calloc(size - mid, sizeof(int));
+
+    for (int i = 0; i < mid; i++) {
+        left[i] = values[i];
+    }
+    for (int i = mid; i < size; i++) {
+        right[i - mid] = values[i];
+    }
+
+    mergesort(mid, left);
+    mergesort(size - mid, right);
+
+    merge(left, right, values, mid, size - mid);
+
+    free(left);
+    free(right);
 }
